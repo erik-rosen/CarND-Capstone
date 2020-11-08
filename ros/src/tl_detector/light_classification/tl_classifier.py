bd84974fa4586e1d66cb6b6eaf67d6c09d361aa7
Bulat Yapparov <byapparov@gmail.com>
2020-11-08 09:41:18 +0000

    Fixing the tensorflow API problem

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -82,7 +82,7 @@ class TLClassifier(object):
             # Line below is different from tensorflow 2 version used in Jupyter
             # https://stackoverflow.com/questions/57614436/od-graph-def-tf-graphdef-attributeerror-module-tensorflow-has-no-attribut
             od_graph_def = tf.GraphDef()
-            with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
